2e93f92b7453b5b4ba6de5d9771cd70258479cb1
Teena Goyal <teenagoyal627>
2021-09-25 23:26:06 +0530

    Practice of Escapes sequences

---
+++ PracticeOfEscapeSquences.c
@@ -0,0 +1,26 @@
+ #include<stdio.h>
+   maintin()
+ {
+     printf("To print my name\t");
+     printf("My name is Teena Goyal");
+     return 0;
+ }
+
+
+ #include<stdio.h>
+  int main()
+ {
+      printf("To print my name \b     ");
+    printf("My name is Teena Goyal");
+    return 0;
+}
+
+
+
+ #include<stdio.h>
+   int main()
+{
+        printf("To print my name\r");
+         printf("My name is Teena Goyal");
+         return 0;
+ }
