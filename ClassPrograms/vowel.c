654e81094ad043cb18510925b3cf251bd167c205
Teena Goyal <teenagoyal627>
2022-01-26 13:13:31 +0530

    vowel

---
+++ ClassPrograms/vowel.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    char p,a,e,i,o,u;
+    printf("Enter a character  ");
+    scanf("%c",&p);
+    if(p==a)
+    printf("vowel");
+    else if (p==e)
+    printf("vowel");
+    else if (p==i)
+    printf("vowel");
+    else if (p==o)
+    printf("vowel");
+    else if (p==u)
+    printf("vowel");
+    else
+    printf("not vowel");
+    return 0;
+
+}
