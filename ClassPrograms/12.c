243eb5d8d2567b98d7a46c1a329351d87bc0bb4b
Teena Goyal <teenagoyal627>
2022-02-18 10:39:18 +0530

    change in condition of j

--- ClassPrograms/12.c
+++ ClassPrograms/12.c
@@ -6,7 +6,7 @@ int main()
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=1;j<=n;j--)
+        for(j=1;j<=n;j++)
         {
         printf("%d",j);
     }
---
+++ ClassPrograms/Nested/practice.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int j,i,a;
+//char ch;
+printf("Enter the no.  ");
+scanf("%d",&a);
+for(i=1;i<=a;i++)
+{
+    for(j=1;j>=1;j--)
+    {
+        printf("%d",j);
+    }
+
+
+    printf("\n");
+}
+
+return 0;
+}
