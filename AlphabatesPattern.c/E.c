3fc4313ac60c6583bc89fc58dd0b137c09ef244c
Teena Goyal <teenagoyal627>
2022-02-20 12:44:25 +0530

    pattern program

---
+++ AlphabatesPattern.c/E.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int i,j,n;
+printf("Enter the number  ");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+    for(j=1;j<=n;j++)
+    {
+        if(i==1||i==n||i==n/2+1||j==1)
+        {
+            printf("*");
+        }
+        else
+        printf(" ");
+    }
+    printf("\n");
+}
+
+return 0;
+}
