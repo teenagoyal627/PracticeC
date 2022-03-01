6ededb49f0421cce9f6aa7eac2c5551601b74239
Teena Goyal <teenagoyal627>
2022-03-01 17:54:31 +0530

    use of escape specifer

--- array.c/2-DArray.c/BePraticalWithC.c/StudentDetails.c
+++ array.c/2-DArray.c/BePraticalWithC.c/StudentDetails.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main()
 {
-
-printf("Name of student\tTeena Goyal");
+int a;
+printf("\'Name of student=Teena Goyal\'");
 printf("\nFather's Name\tMr.BrijMohan Goyal");
 printf("\nYear\t1st Year");
+printf("\n%d",a);//It takes garbage value
 return 0;
 }
