144fb4a874c1359d1c1e8dd139b5271ede2514b7
Teena Goyal <teenagoyal627@gmail.com>
2022-04-03 18:25:43 +0530

    Number is zero or not

---
+++ array.c/2-DArray.c/BePraticalWithC.c/NumberIsZeroOrNot.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("Enter the number  ");
+scanf("%d",&a);
+if(a==0)
+printf("Enter number is zero  ");
+else
+printf("Enter number is not zero  ");
+return 0;
+}
---
+++ array.c/2-DArray.c/BePraticalWithC.c/StatementWithoutSemicolon.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#define decode(s,t,u,p,e,d) m##s##u##t
+#define begin decode(a,n,i,m,a,t,e)
+int begin()
+{
+    printf("Hello Developers  ")
+
+
+}
