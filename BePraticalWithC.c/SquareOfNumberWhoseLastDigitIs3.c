7e1392411870fe0a8f857e0f5c8a549ed1c42572
Teena Goyal <teenagoyal627@gmail.com>
2022-04-17 12:08:11 +0530

    last digit 3 or not

---
+++ BePraticalWithC.c/SquareOfNumberWhoseLastDigitIs3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a; 
+printf("Enter the number   ");
+scanf("%d",&a);
+if(a%10==3)
+printf("square of number is %d",a);
+else
+printf("Invalid number  ");
+
+return 0;
+}
