b6300cb6e8edf981bd795ac4a8cf69b6b7b06cf3
Teena Goyal <teenagoyal627@gmail.com>
2022-04-17 11:45:08 +0530

    length of character

---
+++ 2-DArray/LowerCaseLetter.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[20]; 
+int l;
+printf("Enter a name which you want to convert in lower case  ");
+gets(a);
+l=strlen(a);
+printf("%d",l);
+return 0;
+}
