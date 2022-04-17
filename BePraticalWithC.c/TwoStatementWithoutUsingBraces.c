47303934127972e9ab2704b71b7a22e823637ff1
Teena Goyal <teenagoyal627@gmail.com>
2022-04-17 12:03:39 +0530

    Using If

---
+++ BePraticalWithC.c/TwoStatementWithoutUsingBraces.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("Enter the number  ");
+scanf ("%d",&a);
+if(a<40)
+printf("number is less than 40");
+printf("\nnumber is greater than 40");
+return 0;
+}
