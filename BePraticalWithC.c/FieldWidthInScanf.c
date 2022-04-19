c4cd8de69ec268b83a7721afc77bf32f6b9b665a
Teena Goyal <teenagoyal627@gmail.com>
2022-04-19 22:07:36 +0530

    Field width in scanf

---
+++ BePraticalWithC.c/FieldWidthInScanf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("Enter two number of 4 digit number   ");
+scanf("%3d%4d",&a,&b);
+printf("Enter one more number  \n");
+scanf("%3d",&c);
+printf("you entered   ");
+printf("a=%d\tb=%d\tc=%d",a,b,c);
+
+return 0;
+}
