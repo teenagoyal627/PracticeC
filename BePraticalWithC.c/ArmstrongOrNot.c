13f1ca28d939629975caee5fc11eb0bcbfbf9bf2
Teena Goyal <teenagoyal627@gmail.com>
2022-04-20 18:15:24 +0530

    added loop

--- BePraticalWithC.c/ArmstrongOrNot.c
+++ BePraticalWithC.c/ArmstrongOrNot.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
 int main()
 {
-int n,s,r,t; 
+int n,s,r,t,i; 
 printf("Enter the number   ");
 scanf("%d",&n);
 n=t;
 s=0;
+while(i<1000)
+{
+    i=n;
 while(n>0)
 {
     r=n%10;
     s=s+r*r*r;
     n=n/10;
+}
 }
 if(s==t)
 {
