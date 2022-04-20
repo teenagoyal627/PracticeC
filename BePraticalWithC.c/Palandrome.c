c053434c59992a984934179c0c70c85691d787a6
Teena Goyal <teenagoyal627@gmail.com>
2022-04-20 17:41:15 +0530

    Number is reverse or palandrome or not

---
+++ BePraticalWithC.c/Palandrome.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n,r,rev,t;        
+printf("Enter the number  ");
+scanf("%d",&n);
+rev=0;
+t=n;
+while(n>0)
+{
+    r=n%10;
+    rev=rev*10+r;
+    n=n/10;
+}
+printf("Reverse of number is %d",rev);
+if (rev==t)
+printf("\nNumber is palandrome  ");
+return 0;
+}
