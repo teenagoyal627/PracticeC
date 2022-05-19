9b0a3751ed99e967f564b0f64d7e81028434284e
Teena Goyal <teenagoyal627@gmail.com>
2022-05-19 19:23:05 +0530

    Use of more one function

---
+++ Function/UseOfMoreThanTwofunction.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    void fun1(void);
+    void fun2(void);
+   printf("Hello Teena ");
+fun1();
+fun2();
+return 0;
+}
+   void fun1()
+   {
+       printf("\nHow are you  ");
+   }
+   void fun2()
+   {
+       printf("\nYou are good or not  ");
+   }
+
