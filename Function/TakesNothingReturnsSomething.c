47acc41543476b157e808a0df1c42684b2ca1655
Teena Goyal <teenagoyal627@gmail.com>
2022-05-19 19:56:16 +0530

    Takes something return nothing

--- Function/TakesNothingReturnsSomething.c
+++ Function/TakesNothingReturnsSomething.c
@@ -6,11 +6,12 @@ int main()
 printf("Enter the numbers  ");
 scanf("%d,&a");
 sum (a);
-//printf("%d",a);
+printf("%d",a);
 return 0;
 }
 void sum (int x)
 {
    // return(x);
    printf("U entered = %d\n",x);
+   x=x+10;
 }
